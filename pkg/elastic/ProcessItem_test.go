74d2cc68a1df03d6e97274b3c4a60a3d6b10d429
Sam Caldwell <github@samcaldwell.net>
2024-12-21 22:53:18 -0600

    Add test for ProcessItem()

---
+++ pkg/elastic/ProcessItem_test.go
@@ -0,0 +1,37 @@
+package elastic
+
+import (
+	"github.com/google/uuid"
+	"github.com/sam-caldwell/splunk-elastic-addon/pkg/data"
+	"sync"
+	"testing"
+)
+
+// TestProcessItem tests the ProcessItem function.
+func TestProcessItem(t *testing.T) {
+	t.Run("HappyPath", func(t *testing.T) {
+		traceId := uuid.New()
+		item := data.Item{
+			ElasticHost:  "http://localhost:9200",
+			ElasticIndex: "test-index",
+			QueryString:  "query",
+		}
+		var wg sync.WaitGroup
+		wg.Add(1)
+		ProcessItem(traceId, 1, item, &wg)
+		wg.Wait()
+	})
+
+	t.Run("ErrorCreatingClient", func(t *testing.T) {
+		traceId := uuid.New()
+		item := data.Item{
+			ElasticHost:  "",
+			ElasticIndex: "test-index",
+			QueryString:  "query",
+		}
+		var wg sync.WaitGroup
+		wg.Add(1)
+		ProcessItem(traceId, 1, item, &wg)
+		wg.Wait()
+	})
+}
